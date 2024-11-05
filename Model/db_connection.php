02a4de59ae065cfec1b1a47a699ca396f3a9f781
juliana <jul.ata1212@gmail.com>
2024-11-05 16:50:42 +0200

    databaseConnection

---
+++ Model/db_connection.php
@@ -0,0 +1,9 @@
+<?php
+$config = require 'configurations.php';
+
+$conn = new mysqli($config['DB_HOST'], $config['DB_USER'], $config['DB_PASS'], $config['DB_NAME']);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
