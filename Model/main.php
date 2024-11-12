6ec355e97fd9b89c5ffbcb0ab9248b881568b0d3
juliana <jul.ata1212@gmail.com>
2024-11-12 04:35:54 +0200

    bla

---
+++ Model/main.php
@@ -0,0 +1,30 @@
+<?php
+require_once 'DatabaseConnection.php';
+require_once 'IEvent.php';
+require_once 'Event.php';
+require_once 'CampaignStrategy.php';
+require_once 'VolunteeringEventStrategy.php';
+
+function main() {
+
+    $eventID = 1; 
+    $eventTime = new DateTime('2024-12-01 10:00:00'); 
+    $location = "City Park"; 
+    $volunteersNeeded = 5; 
+
+    $campaignEvent = new Event($eventTime, $location, $volunteersNeeded, $eventID, new CampaignStrategy()); 
+    $volunteeringEvent = new Event($eventTime, $location, $volunteersNeeded, $eventID, new VolunteeringEventStrategy()); 
+    $donorID = 101;
+
+    echo "---- Testing CampaignStrategy ----\n";
+    $campaignEvent->signUpBasedOnStrategy($donorID);
+    echo "----------------------------------\n";
+
+    echo "---- Changing to VolunteeringEventStrategy ----\n";
+    $campaignEvent->setStrategy(new VolunteeringEventStrategy());
+    $campaignEvent->signUpBasedOnStrategy($donorID); 
+    echo "-------------------------------------------\n";
+}
+
+main();
+?>
