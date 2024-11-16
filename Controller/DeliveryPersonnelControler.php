8f358d7259027a8f469841127ac62392b55e3516
Seifeldin2510 <seifeldin.ashraf@yahoo.com>
2024-11-16 16:08:51 +0200

    Admin controller updated

--- Controller/DeliveryPersonnelControler.php
+++ Controller/DeliveryPersonnelControler.php
@@ -3,18 +3,6 @@
 require_once '../Model/delivery.php';
 class DeliveryPersonnelController {
 
-    // Method to schedule delivery
-    // public function scheduleDelivery($params) {
-    //     // Logic to schedule a delivery
-    //     return $params; // Return the scheduled delivery info or status
-    // }
-
-    // // Method to track deliveries
-    // public function trackDeliveries($params) {
-    //     // Logic to track deliveries
-    //     return $params; // Return delivery tracking info or status
-    // }
-
     public function updateDestination($deliveryId): void {
         $delivery= Delivery::retrieve($deliveryId);
         $destination=$delivery->getDestination();
