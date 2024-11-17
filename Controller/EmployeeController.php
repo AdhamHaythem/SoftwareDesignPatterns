541bd25838d5f53d5e0947a0f966c6d053326c8e
YoussefElNagy <youssef.nagy2002@gmail.com>
2024-11-17 03:06:13 +0200

    Modified DM+Employee

--- Controller/EmployeeController.php
+++ Controller/EmployeeController.php
@@ -2,14 +2,26 @@
 require_once '../Model/EmployeeModel.php';
 require_once './UserController.php';
 require_once '../Model/UserModel.php';
+require_once '../Model/hr.php';
+require_once '../Model/technical.php';
+require_once '../Model/delivery.php';
 
 class EmployeeController {
-    public function requestLeave($time, $reason): bool {
-        return EmployeeModel::requestLeave($time, $reason); 
+    public function requestLeave($time, $reason,$type,$employeeId): bool {
+         if($type=="HR") $employee= hrModel::retrieve($employeeId);
+         elseif($type=="Delivery") $employee= DeliveryPersonnel::retrieve($employeeId);
+         elseif($type=="Technical") $employee= technicalModel::retrieve($employeeId);
+        if($employee){
+            return $employee->requestLeave($time, $reason);
+        }
+        return false;
     }
 
-    public function reportHoursWorked(int $userId, int $hours): void {
-        $employee = EmployeeModel::retrieve($userId);
+    public function reportHoursWorked(int $employeeId,int $hours,string $type): void {
+        if($type=="HR") $employee= hrModel::retrieve($employeeId);
+        elseif($type=="Delivery") $employee= DeliveryPersonnel::retrieve($employeeId);
+        elseif($type=="Technical") $employee= technicalModel::retrieve($employeeId);
+       
         if ($employee) {
             $employee->reportHoursWorked($hours);
         }
@@ -20,13 +32,13 @@ $employeeController = new EmployeeController();
 
 if (isset($_POST['requestLeave'])) {
     if (isset($_POST['time'], $_POST['reason'])) {
-        $employeeController->requestLeave($_POST['time'], $_POST['reason']);
+        $employeeController->requestLeave($_POST['time'], $_POST['reason'],$_POST['type'], $_POST['employeeId']);
     }
 }
 
 if (isset($_POST['reportHoursWorked'])) {
     if (isset($_POST['userId'], $_POST['hours'])) {
-        $employeeController->reportHoursWorked($_POST['userId'], (int)$_POST['hours']);
+        $employeeController->reportHoursWorked($_POST['employeeId'], (int)$_POST['hours'],$_POST['type']);
     }
 }
 
