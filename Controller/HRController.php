5517f61265b8d811672c6077026370627e46da59
Omar_Ashraf <147910629+OmarAshraf166@users.noreply.github.com>
2025-01-16 18:43:56 +0200

    Update HRController.php (remove errors lines)

--- Controller/HRController.php
+++ Controller/HRController.php
@@ -1,21 +1,22 @@
 <?php
-require_once "../View/AdminView.php";
-class HRController {
+// require_once "../View/AdminView.php";
+// class HRController {
     
-    public function getReports($hr) {
-        $reports = $hr->getReports();
-        $x = new AdminView();
-        $x->displayReports($reports);
+//     public function getReports($hr) {
+//         $reports = $hr->getReports();
+//         $x = new AdminView();
+//         $x->displayReports($reports);
         
-    }
+//     }
 
-}
+// }
 
- // HRController actions
-    $hrController = new HRController();
-    if (isset($_POST['getReports'])) {
-        $hr = HRModel::retrieve($_POST['userId']);
-        $hrController->getReports($hr);
-    } 
+//  // HRController actions
+//     $hrController = new HRController();
+//     if (isset($_POST['getReports'])) {
+//         $hr = HRModel::retrieve($_POST['userId']);
+//         $hrController->getReports($hr);
+//     } 
     
 ?>
+
